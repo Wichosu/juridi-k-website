66c73188253b1bfc35fb536fa51394a52022be73
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-01-05 23:29:34 -0600

    working on i18n

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -2,12 +2,17 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import type { Metadata } from 'next'
 import { Noto_Serif_SC } from 'next/font/google'
 import { Noto_Sans_SC } from 'next/font/google'
-import './globals.css'
+import { dir } from 'i18next'
+import { languages } from '../i18n/settings'
 
 const noto_serif_sc = Noto_Serif_SC({ 
   weight: ['400', '500', '600', '700'],
   subsets: ['latin']
 })
+
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }))
+}
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,11 +21,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: {
+    lng
+  }
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  params: {
+    lng: string
+  }
 }) {
   return (
-    <html lang="en">
+    <html lang={lng} dir={dir(lng)}>
       <body className={noto_serif_sc.className}>
         <AppRouterCacheProvider>
           {children}
