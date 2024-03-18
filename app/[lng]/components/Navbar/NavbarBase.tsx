59354a9eb625822a644ac17c420996afb38d6a76
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-03-17 21:24:26 -0600

    More space between services on navbar

--- app/[lng]/components/Navbar/NavbarBase.tsx
+++ app/[lng]/components/Navbar/NavbarBase.tsx
@@ -21,14 +21,14 @@ export const NavbarBase = ({ t, lng }: Props) => {
     <nav className="flex justify-between px-8 py-4 bg-darkblue">
       <Image src={LogoBig} width={100} height={100} alt="juridi-k logo" className="cursor-pointer" />
       <Image src={BurgerIcon} width={40} height={40} alt="menu button" className="cursor-pointer" onClick={() => setShowNavbar((val) => !val)}  />
-      <aside className={`fixed flex flex-col gap-4 top-0 right-0 w-3/4 h-screen px-8 pt-12 bg-darkblue transition ease-in-out ${showNavbar? 'translate-x-0' : 'translate-x-full'}`}>
+      <aside className={`fixed overflow-scroll flex flex-col gap-4 top-0 right-0 w-3/4 h-screen px-8 py-12 bg-darkblue transition ease-in-out ${showNavbar? 'translate-x-0' : 'translate-x-full'}`}>
         <Image src={CloseIcon} width={25} height={25} alt="close button" className="cursor-pointer self-end" onClick={() => setShowNavbar((val) => !val)} />
         <Link href={'/'} className="text-white">{ t("home") }</Link>
         <div className="flex justify-between cursor-pointer" onClick={() => setShowServices((val) => !val)}>
           <p className="text-white">{ t("services") }</p>
           <Image src={ExpandIcon} width={20} height={20} alt="Expand list of services" className={showServices ? 'rotate-180' : 'rotate-0'} />
         </div>
-        <ul className={`text-white ml-4 ${showServices ? 'block' : 'hidden'}`}>
+        <ul className={`grid gap-3 text-white ml-4 ${showServices ? 'block' : 'hidden'}`}>
           <li>{ t("protection") }</li>
           <li>{ t("private_equity_advisory") }</li>
           <li>{ t("bankruptcy_proceedings_and_restructuring") }</li>
@@ -48,7 +48,6 @@ export const NavbarBase = ({ t, lng }: Props) => {
           <li>{ t("trademark_registrations") }</li>
           <li>{ t("social_security") }</li>
           <li>{ t("insurance_and_insurers") }</li>
-          <li>{ t("lawyers") }</li>
         </ul>
         <Link href={'/'} className="text-white">Nuestros Abogados</Link>
         <LanguageSwitcher lng={lng} />
