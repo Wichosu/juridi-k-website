a29629a111cf00e1d1cec84504c312fe863b7e63
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-03-18 20:35:39 -0600

    Working on Hero

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -3,22 +3,19 @@ import Link from 'next/link'
 import { useTranslation } from '../i18n/client'
 import { Footer } from './components/Footer/client'
 import { Navbar } from './components/Navbar/client'
+import { Hero } from './components/Hero/client'
 
 interface Props {
   params: { lng: string}
 }
 
 export default function Page({ params: { lng } }: Props) {
-  const { t } = useTranslation(lng)
+  const { t } = useTranslation(lng, "home-page")
 
   return (
     <>
       <Navbar lng={lng} />
-      <h1>{t('title')}</h1>
-      <Link href={`/${lng}/second-page`}>
-        {t('to-second-page')}
-      </Link>
-      <Footer lng={lng} />
+      <Hero lng={lng} />
     </>
   )
 }
