a9bf1f9e888cc6e6a17d497b1f5f9827439344c3
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-04-16 18:58:05 -0600

    working on contact styles

--- app/[lng]/components/ContactUs/ContactUsBase.tsx
+++ app/[lng]/components/ContactUs/ContactUsBase.tsx
@@ -7,8 +7,14 @@ interface Props {
 
 export const ContactUsBase = ({ t, lng }: Props) => {
   return (
-    <>
-      hello there
-    </>
+    <div className="bg-darkblue grid gap-4 px-8 py-4">
+      <h1 className="text-white text-3xl">Contactanos</h1>
+      <textarea className="h-auto" rows={1} cols={10}></textarea>
+      <input className="" placeholder="nombre" />
+      <input className="" placeholder="email" />
+      <input className="" placeholder="telefono" />
+      <input className="" placeholder="mensaje" />
+      <button className="w-fit bg-darkred text-white px-4 py-2">Enviar</button>
+    </div>
   )
 }
