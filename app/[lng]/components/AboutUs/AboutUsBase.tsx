10298b97a18d8501a315e288b0dc2ea65dd3fbbc
wicho <54341712+Wichosu@users.noreply.github>
2024-06-24 16:47:29 -0600

    About us component view for desktop

--- app/[lng]/components/AboutUs/AboutUsBase.tsx
+++ app/[lng]/components/AboutUs/AboutUsBase.tsx
@@ -10,15 +10,25 @@ interface Props {
 
 export const AboutUsBase = ({ t, lng }) => {
   return (
-    <section className="px-8 py-4 grid gap-4">
-      <h1 className="text-3xl py-4">{ t("title") }</h1>
-      <p className="text-sm">{ t("description") }</p>
-      <div className="bg-darkred text-white flex justify-around py-4 my-4 cursor-pointer">
-        <div>
-          <p className="text-xs">{ t("button_subheader") }</p>
-          <h2 className="text-xl">{ t("button_header") }</h2>
+    <section className="
+        px-8 py-4 grid gap-4 lg:max-w-screen-xl lg:mx-auto lg:flex lg:flex-row-reverse
+        lg:justify-between
+      "
+    >
+      <div>
+        <h1 className="text-3xl py-4">{ t("title") }</h1>
+        <p className="text-sm">{ t("description") }</p>
+        <div className="
+            bg-darkred text-white flex justify-around py-4 my-4 cursor-pointer
+            lg:w-fit lg:px-8
+          "
+        >
+          <div className="lg:mr-8">
+            <p className="text-xs">{ t("button_subheader") }</p>
+            <h2 className="text-xl">{ t("button_header") }</h2>
+          </div>
+          <Image src={RightArrow} width={35} height={35} alt="Make an appointment" />
         </div>
-        <Image src={RightArrow} width={35} height={35} alt="Make an appointment" />
       </div>
       <Image src={Lawyers} width={500} height={500} alt="" />
     </section>
