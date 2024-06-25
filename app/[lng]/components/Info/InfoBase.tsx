5e5ce55dc1a6f9e4e12d0b6d81bede3b481869fa
wicho <54341712+Wichosu@users.noreply.github>
2024-06-25 16:49:39 -0600

    Information component made responsive

--- app/[lng]/components/Info/InfoBase.tsx
+++ app/[lng]/components/Info/InfoBase.tsx
@@ -8,29 +8,31 @@ interface Props {
 
 export const InfoBase = ({ t, lng}: Props) => {
   return (
-    <section className="px-8 my-4">
-      <Accordion lng={lng} locale="info" name={ t("justification") }>
-        JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
+    <section>
+      <div className="px-8 my-4 lg:max-w-screen-xl lg:mx-auto">
+        <Accordion lng={lng} locale="info" name={ t("justification") }>
+          JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
 que son conscientes de la necesidad de prepararse académicamente para
 desarrollarse en una rama específica del Derecho para brindar un servicio de
 calidad a todo aquel que requiera de consultoría y asesoría jurídica. 
-      </Accordion>
-      <Accordion lng={lng} locale="info" name={ t("mision") }>
-        JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
+        </Accordion>
+        <Accordion lng={lng} locale="info" name={ t("mision") }>
+          JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
 que son conscientes de la necesidad de prepararse académicamente para
 desarrollarse en una rama específica del Derecho para brindar un servicio de
 calidad a todo aquel que requiera de consultoría y asesoría jurídica. 
-      </Accordion>
-      <Accordion lng={lng} locale="info" name={ t("vision") }>
-        JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
+        </Accordion>
+        <Accordion lng={lng} locale="info" name={ t("vision") }>
+          JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
 que son conscientes de la necesidad de prepararse académicamente para
 desarrollarse en una rama específica del Derecho para brindar un servicio de
-      </Accordion>
-      <Accordion lng={lng} locale="info" name={ t("values") }>
-        JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
+        </Accordion>
+        <Accordion lng={lng} locale="info" name={ t("values") }>
+          JURIDI-K nace a partir de la idea de un grupo de abogados de amplia experiencia
 que son conscientes de la necesidad de prepararse académicamente para
 desarrollarse en una rama específica del Derecho para brindar un servicio de
-      </Accordion>
+        </Accordion>
+      </div>
     </section>
   )
 }
