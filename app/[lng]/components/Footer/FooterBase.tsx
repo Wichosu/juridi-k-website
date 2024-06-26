e68a1e4977aa14482599a97d5e2405a888c07442
wicho <54341712+Wichosu@users.noreply.github>
2024-06-26 13:53:45 -0600

    Footer component made responsive

--- app/[lng]/components/Footer/FooterBase.tsx
+++ app/[lng]/components/Footer/FooterBase.tsx
@@ -9,15 +9,21 @@ import Whatsapp from '@/public/whatsapp.svg'
 
 export const FooterBase = ({ t, lng }) => {
   return (
-    <footer className='grid gap-4 px-8 py-4 bg-darkblue text-white'>
-      <ul className='flex justify-between'>
-        <li><Image src={Envelope} width={40} height={40} alt='' /></li>
-        <li><Image src={Telephone} width={40} height={40} alt='' /></li>
-        <li><Image src={Whatsapp} width={40} height={40} alt='' /></li>
-      </ul>
-      <p>JURIDI-K: Helsinki No. 2. Colonia Monte Magno, Xalapa-Veracruz, México. C.P. 91193.</p>
-      <Image className='mx-auto' src={Logo} width={200} height={200} alt='' />
-      <p>Todos los derechos reservados JURIDI-K 2024 ®</p>
+    <footer className='bg-darkblue text-white'>
+      <div className='grid gap-4 px-8 py-4 lg:max-w-screen-xl lg:mx-auto'>
+        <div className='lg:flex lg:flex-row-reverse lg:justify-evenly lg:mb-4'>
+          <div className='lg:max-w-lg lg:grid lg:gap-8'>
+            <ul className='flex justify-between'>
+              <li className='cursor-pointer'><Image src={Envelope} width={40} height={40} alt='' /></li>
+              <li className='cursor-pointer'><Image src={Telephone} width={40} height={40} alt='' /></li>
+              <li className='cursor-pointer'><Image src={Whatsapp} width={40} height={40} alt='' /></li>
+            </ul>
+            <p>JURIDI-K: Helsinki No. 2. Colonia Monte Magno, Xalapa-Veracruz, México. C.P. 91193.</p>
+          </div>
+          <Image className='mx-auto lg:m-0' src={Logo} width={200} height={200} alt='' />
+        </div>
+        <p className='lg:text-center'>Todos los derechos reservados JURIDI-K 2024 ®</p>
+      </div>
     </footer>
   )
 }
