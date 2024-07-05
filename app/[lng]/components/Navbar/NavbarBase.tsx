86de1c03ea19cfdd4143acc6361be472c0b996a6
wicho <54341712+Wichosu@users.noreply.github>
2024-07-05 06:39:14 -0600

    Fetch services to navbar component

--- app/[lng]/components/Navbar/NavbarBase.tsx
+++ app/[lng]/components/Navbar/NavbarBase.tsx
@@ -4,9 +4,11 @@ import LogoBig from "../../../../public/juridi-k-logo.svg"
 import BurgerIcon from "../../../../public/list.svg"
 import ExpandIcon from "../../../../public/caret-down-fill.svg"
 import CloseIcon from "../../../../public/x-circle.svg"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { LanguageSwitcher } from "./components/LanguageSwitcher/client"
 import { TFunction } from "i18next"
+import { sanityFetch } from "@/sanity/lib/client"
+import { SanityDocument } from "next-sanity"
 
 interface Props {
   t: TFunction<any, undefined>,
@@ -16,6 +18,38 @@ interface Props {
 export const NavbarBase = ({ t, lng }: Props) => {
   const [showServices, setShowServices] = useState(false)
   const [showNavbar, setShowNavbar] = useState(false)
+  const [services, setServices] = useState<SanityDocument[]>()
+
+  useEffect(() => {
+    let isFetched = false
+
+    const fetchData = async () => {
+      const FETCH_QUERY = `*[_type == "service" && language == "${lng}"]{
+        _id,
+        title,
+        slug,
+        language,
+        "_translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{
+          title,
+          slug,
+          language
+        },
+      }`;
+
+      const fetchData = await sanityFetch<SanityDocument[]>({query: FETCH_QUERY})
+
+      if(!isFetched) {
+        setServices(fetchData)
+      }
+    }
+
+    fetchData()
+      .catch(console.error)
+
+    return () => {
+      isFetched = true
+    }
+  }, [])
 
   return (
     <nav className="bg-darkblue lg:relative">
@@ -36,27 +70,19 @@ export const NavbarBase = ({ t, lng }: Props) => {
           </div>
           <ul className={`
               grid gap-3 text-white ml-4 ${showServices ? 'block' : 'hidden'}
-              lg:absolute lg:-left-40 lg:top-20 lg:bg-darkblue lg:grid-cols-2 
+              lg:absolute lg:-left-0 lg:top-20 lg:bg-darkblue lg:grid-cols-2 
               `}>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("protection") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("private_equity_advisory") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("bankruptcy_proceedings_and_restructuring") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("administrative_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("enviromental_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("civil_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("corporate_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("family_right") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("tax_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("human_rights") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("labor_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("commercial_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("criminal_law") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("fusions_and_acquisitions") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("expert_reports") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("intellectual_property") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("trademark_registrations") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("social_security") }</li>
-            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("insurance_and_insurers") }</li>
+            {
+              services?.map((service, key) => (
+                <Link 
+                  key={key}
+                  className="px-4 py-1 hover:bg-darkred hover:cursor-pointer"
+                  href={`/${service?.slug.current}`}
+                >
+                  { service?.title }
+                </Link>
+              ))
+            }
           </ul>
           <Link href={'/'} className="text-white">Nuestros Abogados</Link>
           <LanguageSwitcher lng={lng} />
