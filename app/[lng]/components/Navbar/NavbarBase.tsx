183677cb6ac1477601a0b16717c674456396e6a4
wicho <54341712+Wichosu@users.noreply.github>
2024-07-05 06:08:44 -0600

    Fix navbar style issues

--- app/[lng]/components/Navbar/NavbarBase.tsx
+++ app/[lng]/components/Navbar/NavbarBase.tsx
@@ -18,15 +18,15 @@ export const NavbarBase = ({ t, lng }: Props) => {
   const [showNavbar, setShowNavbar] = useState(false)
 
   return (
-    <nav className="bg-darkblue">
-      <div className="flex justify-between px-8 py-4 capitalize lg:max-w-screen-xl lg:mx-auto">
+    <nav className="bg-darkblue lg:relative">
+      <div className="flex justify-between px-8 py-4 capitalize lg:max-w-screen-xl lg:mx-auto lg:relative">
         <Image src={LogoBig} width={100} height={100} alt="juridi-k logo" className="cursor-pointer" />
         <Image src={BurgerIcon} width={40} height={40} alt="menu button" className="cursor-pointer lg:hidden" onClick={() => setShowNavbar((val) => !val)}  />
         <aside className={`
             fixed overflow-scroll flex flex-col gap-4 top-0 right-0 w-3/4 h-screen px-8 py-12
             bg-darkblue transition ease-in-out 
             ${showNavbar? 'translate-x-0 lg:translate-x-0' : 'translate-x-full lg:translate-x-0'}
-            lg:relative lg:flex-row lg:bg-none lg:h-fit lg:w-fit
+            lg:relative lg:flex-row lg:bg-none lg:h-fit lg:w-fit lg:overflow-visible
             `}>
           <Image src={CloseIcon} width={25} height={25} alt="close button" className="cursor-pointer self-end lg:hidden" onClick={() => setShowNavbar((val) => !val)} />
           <Link href={'/'} className="text-white">{ t("home") }</Link>
@@ -36,27 +36,27 @@ export const NavbarBase = ({ t, lng }: Props) => {
           </div>
           <ul className={`
               grid gap-3 text-white ml-4 ${showServices ? 'block' : 'hidden'}
-              lg:absolute lg:top-20 lg:left-0 lg:h-fit lg:w-fit lg:z-10 
+              lg:absolute lg:-left-40 lg:top-20 lg:bg-darkblue lg:grid-cols-2 
               `}>
-            <li>{ t("protection") }</li>
-            <li>{ t("private_equity_advisory") }</li>
-            <li>{ t("bankruptcy_proceedings_and_restructuring") }</li>
-            <li>{ t("administrative_law") }</li>
-            <li>{ t("enviromental_law") }</li>
-            <li>{ t("civil_law") }</li>
-            <li>{ t("corporate_law") }</li>
-            <li>{ t("family_right") }</li>
-            <li>{ t("tax_law") }</li>
-            <li>{ t("human_rights") }</li>
-            <li>{ t("labor_law") }</li>
-            <li>{ t("commercial_law") }</li>
-            <li>{ t("criminal_law") }</li>
-            <li>{ t("fusions_and_acquisitions") }</li>
-            <li>{ t("expert_reports") }</li>
-            <li>{ t("intellectual_property") }</li>
-            <li>{ t("trademark_registrations") }</li>
-            <li>{ t("social_security") }</li>
-            <li>{ t("insurance_and_insurers") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("protection") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("private_equity_advisory") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("bankruptcy_proceedings_and_restructuring") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("administrative_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("enviromental_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("civil_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("corporate_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("family_right") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("tax_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("human_rights") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("labor_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("commercial_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("criminal_law") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("fusions_and_acquisitions") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("expert_reports") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("intellectual_property") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("trademark_registrations") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("social_security") }</li>
+            <li className="px-4 py-1 hover:bg-darkred hover:cursor-pointer">{ t("insurance_and_insurers") }</li>
           </ul>
           <Link href={'/'} className="text-white">Nuestros Abogados</Link>
           <LanguageSwitcher lng={lng} />
